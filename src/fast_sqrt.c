28b5913622b86b50acfddcd43199f13fe483c4f8
Eugene Hong <awidesky@naver.com>
2024-12-14 12:08:20 +0900

    add comment

--- src/fast_sqrt.c
+++ src/fast_sqrt.c
@@ -2,10 +2,13 @@
 #include <stdint.h>
 
 float fast_sqrtf(float f) {
-	int32_t i = *(int32_t *)&f;
+	int32_t i = *(int32_t *)&f; //Evil floating-point bit level hacking
+	//shift exponent to make it roughly sqrt(x),
+	//and add magic number to adjust mantissa and exponent.
+	//The magic number is found by minimising error, which is |sqrt(x) - (shift right of x)|
 	i = (i >> 1) + 0x1fbb67ae;
 	float f1 = *(float *)&i;
-	return 0.5F * (f1 + f / f1); //TODO : more comment
+	return 0.5F * (f1 + f / f1); //Newton's method 
 }
 
 double fast_sqrtd(double d) {
